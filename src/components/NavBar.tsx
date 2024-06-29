b1f6bb72999e958b670135ac16fbcca2ff39af1e
Himanshu Rai <himanshuraimau9@gmail.com>
2024-06-29 22:30:27 +0530

    make the profile section responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,73 +1,73 @@
-import React from 'react'
-import Link  from 'next/link'
+  import React from 'react'
+  import Link  from 'next/link'
 
-const NavBar = () => {
-  return (
-    <div>
-       <nav className="bg-black text-gray-300 p-4 ">
-      <div className="container mx-5 items-center flex flex-row justify-around ">
-      <Link
-      href="/"
-      className="font-system text-xl font-bold text-center uppercase text-body-light pt-5 pl-10"
-      style={{
-        lineHeight: '8px',
-        letterSpacing: '1.8px',
-        wordSpacing: '0px',
-        textDecoration: 'none solid rgb(67, 133, 215)',
-      }}
-    >
-      <span className="hover:text-blue-400">HIMANSHU RAI</span>
-    </Link>
-
-
-<div className="flex flex-row justify-between space-x-7 mr-20 pt-5">
-      <Link href="#projects"
-        
-          className="font-sans text-[18px] text-body-light hover:text-blue-400"
-          style={{
-            lineHeight: '34px',
-            letterSpacing: '0.9px',
-            fontWeight: 400,
-            textDecoration: 'none solid rgb(67, 133, 215)',
-            textAlign: 'start',
-          }}
-        >
-          Projects
-      
+  const NavBar = () => {
+    return (
+      <div>
+        <nav className="bg-black text-gray-300 p-4 ">
+        <div className="container mx-5 items-center flex flex-row justify-around ">
+        <Link
+        href="/"
+        className="font-system text-xl font-bold text-center uppercase text-body-light pt-5 pl-10"
+        style={{
+          lineHeight: '8px',
+          letterSpacing: '1.8px',
+          wordSpacing: '0px',
+          textDecoration: 'none solid rgb(67, 133, 215)',
+        }}
+      >
+        <span className="hover:text-blue-400">HIMANSHU RAI</span>
       </Link>
-      <Link href="#about"
-          className="font-sans-serif text-[18px] hover:text-blue-400 text-body-light"
-          style={{
-            lineHeight: '34px',
-            letterSpacing: '0.9px',
-            fontWeight: 400,
-            textDecoration: 'none solid rgb(67, 133, 215)',
-            textAlign: 'start',
-          }}
-        >
-          About
-        
-      </Link>
-      <Link href="#blogs"
-        
-          className="font-sans-serif text-[18px] hover:text-blue-400 text-body-light"
-          style={{
-            lineHeight: '34px',
-            letterSpacing: '0.9px',
-            fontWeight: 400,
-            textDecoration: 'none solid rgb(67, 133, 215)',
-            textAlign: 'start',
-          }}
-        >
-          Blogs
-        
-      </Link>
-    </div>
 
+
+  <div className="flex flex-row justify-between space-x-7 mr-20 pt-5">
+        <Link href="#projects"
+          
+            className="font-sans text-[18px] text-body-light hover:text-blue-400"
+            style={{
+              lineHeight: '34px',
+              letterSpacing: '0.9px',
+              fontWeight: 400,
+              textDecoration: 'none solid rgb(67, 133, 215)',
+              textAlign: 'start',
+            }}
+          >
+            Projects
+        
+        </Link>
+        <Link href="#about"
+            className="font-sans-serif text-[18px] hover:text-blue-400 text-body-light"
+            style={{
+              lineHeight: '34px',
+              letterSpacing: '0.9px',
+              fontWeight: 400,
+              textDecoration: 'none solid rgb(67, 133, 215)',
+              textAlign: 'start',
+            }}
+          >
+            About
+          
+        </Link>
+        <Link href="#blogs"
+          
+            className="font-sans-serif text-[18px] hover:text-blue-400 text-body-light"
+            style={{
+              lineHeight: '34px',
+              letterSpacing: '0.9px',
+              fontWeight: 400,
+              textDecoration: 'none solid rgb(67, 133, 215)',
+              textAlign: 'start',
+            }}
+          >
+            Blogs
+          
+        </Link>
       </div>
-    </nav>
-    </div>
-  )
-}
 
-export default NavBar
+        </div>
+      </nav>
+      </div>
+    )
+  }
+
+  export default NavBar
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import {RiTwitterXFill} from "react-icons/ri";
-import { FaInstagram } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import { RiTwitterXFill } from "react-icons/ri";
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Profile = () => {
   return (
-    <div className="flex flex-row items-start justify-center mt-12 text-lg leading-7 space-x-12">
-    
-      <div className="flex justify-start ">
+    <div className="flex flex-col md:flex-row items-center md:items-start justify-center mt-12 text-lg leading-7 space-y-6 md:space-y-0 md:space-x-12">
+      <div className="flex-shrink-0">
         <Image
           src="/profile.jpg"
           alt="Profile Image"
@@ -18,58 +15,56 @@ const Profile = () => {
           className="rounded-full outline outline-offset-1 outline-white"
         />
       </div>
-      
-     
-      <div className="flex flex-col  mt-9">
-        <div className="">
+
+      <div className="flex flex-col items-center md:items-start md:mt-0">
+        <div className="text-center md:text-left">
           <span className="font-bold hover:text-blue-400 text-4xl">
             <a href="/about">Hey, I'm Himanshu.</a>
           </span>
           <br />
           <span className="font-bold text-2xl">I'm a software engineer.</span>
         </div>
-        
-        <nav className="flex items-center mt-4 space-x-8 justify-start">
-        <a
-              href="https://twitter.com/himanshura_i"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center space-x-2"
-            >
-              <RiTwitterXFill style={{color:"white"}} />
-              <span className="text-xl hover:text-white">Twitter</span>
-            </a>
 
-         
+        <nav className="flex items-center mt-4 space-x-8 justify-center md:justify-start">
           <a
-              href="https://www.instagram.com/enghimanshu/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center space-x-2"
-            >
-                <FaInstagram style={{color:"red"}}/>
-              <span className="hover:text-red-700">Instagram</span>
-            </a>
+            href="https://twitter.com/himanshura_i"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center space-x-2"
+          >
+            <RiTwitterXFill style={{ color: "white" }} />
+            <span className="text-xl hover:text-white">Twitter</span>
+          </a>
 
           <a
-              href="https://www.linkedin.com/in/himanshu-rai-246121278/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center space-x-2"
-            >
-                <FaLinkedin style={{color:"blue"}} />
-              <span className="hover:text-blue-700">LinkedIn</span>
-            </a>
-          
+            href="https://www.instagram.com/enghimanshu/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center space-x-2"
+          >
+            <FaInstagram style={{ color: "red" }} />
+            <span className="hover:text-red-700">Instagram</span>
+          </a>
+
           <a
-              href="https://github.com/himanshuraimau"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center space-x-2"
-            >
-                <FaGithub />
-              <span className="text-white-800 hover:text-white-400">Github</span>
-            </a>
+            href="https://www.linkedin.com/in/himanshu-rai-246121278/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center space-x-2"
+          >
+            <FaLinkedin style={{ color: "blue" }} />
+            <span className="hover:text-blue-700">LinkedIn</span>
+          </a>
+
+          <a
+            href="https://github.com/himanshuraimau"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center space-x-2"
+          >
+            <FaGithub />
+            <span className="text-white-800 hover:text-white-400">Github</span>
+          </a>
         </nav>
       </div>
     </div>
