9ee6bd0a3bb6a185326b72018fcbb6cf26d7d25e
Himanshu Rai <himanshuraimau9@gmail.com>
2024-08-19 00:58:02 +0530

    add chages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Profile from "@/components/Profile";
 export default function Home() {
   return (
     <div className="bg-black min-h-screen flex flex-col items-center ">
-      <div className="w-full max-w-4xl p-3 sm:p-6 md:p-10">
+      <div className="w-full max-w-4xl pl-3 pr-3 sm:pl-6 sm:pr-6 md:pl-10 md:pr-10" >
         <NavBar />
         <div className="mt-8">
           <Profile />
