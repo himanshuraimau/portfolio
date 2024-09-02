1a4364ad0bb4d425290c1f4b9ee2e06020e8c39e
Himanshu Rai <himanshuraimau9@gmail.com>
2024-09-02 15:04:24 +0530

    add p

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -22,7 +22,7 @@ const Profile = () => {
             <a href="/about">hey, i'm himanshu.</a>
           </h1>
           <p className="font-mono text-slate-300 text-xl sm:text-2xl max-w-md">
-            i'm a full-stack devloper who loves building cool projects.
+            i'm a full-stack devloper who loves building cool products.
           </p>
         </div>
 
