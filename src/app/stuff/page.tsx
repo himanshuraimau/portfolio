5d223d830ba017e0a52e2f7998828fa2aece516a
Himanshu Rai <himanshuraimau9@gmail.com>
2024-09-07 15:25:14 +0530

    improve responsiveness

--- src/app/stuff/page.tsx
+++ src/app/stuff/page.tsx
@@ -1,12 +1,12 @@
 "use client";
+
 import React, { useEffect, useState } from 'react';
 import { PinContainer } from '@/components/ui/3d-pin';
 import Image from 'next/image';
 
-const Page = ()=> {
+const Page = () => {
   const [isClient, setIsClient] = useState(false);
 
- 
   useEffect(() => {
     setIsClient(true);
   }, []);
@@ -16,7 +16,7 @@ const Page = ()=> {
   }
 
   return (
-    <div className="flex flex-row justify-start items-start flex-wrap gap-2 pt-10 h-screen">
+    <div className="flex flex-row justify-start items-start flex-wrap gap-2 pt-10 h-full">
       <PinContainer
         title="Mood"
         href="https://mood-nine-omega.vercel.app"
@@ -34,8 +34,8 @@ const Page = ()=> {
             <Image
               src="/mood.png"
               alt="Mood Application"
-              layout="fill"
-              objectFit="cover"
+              fill
+              style={{ objectFit: 'cover' }}
               className="rounded-lg"
             />
           </div>
@@ -53,14 +53,14 @@ const Page = ()=> {
             College Nexus
           </h2>
           <p className="text-sm m-0 p-0 font-normal text-slate-500">
-           A Web Application for College Students
+            A Web Application for College Students
           </p>
           <div className="relative flex-1 mt-2 w-full h-full">
             <Image
               src="/college-nexus.png"
               alt="College Nexus Application"
-              layout="fill"
-              objectFit="cover"
+              fill
+              style={{ objectFit: 'cover' }}
               className="rounded-lg"
             />
           </div>
@@ -75,24 +75,22 @@ const Page = ()=> {
       >
         <div className="relative flex flex-col p-3 tracking-tight text-slate-100/50 w-[15rem] h-[13rem]">
           <h2 className="pb-1 m-0 font-bold text-base text-slate-100">
-           Music School
+            Music School
           </h2>
           <p className="text-sm m-0 p-0 font-normal text-slate-500">
-          A Nextjs Frontend Application using Aceternity UI
+            A Nextjs Frontend Application using Aceternity UI
           </p>
           <div className="relative flex-1 mt-2 w-full h-full">
             <Image
               src="/music-school.png"
               alt="Music School Application"
-              layout="fill"
-              objectFit="cover"
+              fill
+              style={{ objectFit: 'cover' }}
               className="rounded-lg"
             />
           </div>
         </div>
       </PinContainer>
-
-      
     </div>
   );
 }
